6a8af797dfd82763e35451026265f0ea8f046d6b
LauraJaime8 <laurajaimev8@gmail.com>
2018-06-19 17:48:40 +0000

    Added library for passwords and add changes in User model

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,12 +15,44 @@ defmodule SimpleAuth.User do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  @required_fields ~w(email)a
+  @required_fields ~w(email)a #~w: devuelve una lista de palabras separada por comas
+                              #a: las palabras de la lista son de tipo atom
   @optional_fields ~w(name is_admin)a
 
+  #Ayuda sobre esto en https://hexdocs.pm/ecto/Ecto.Changeset.html
   def changeset(struct, params \\ %{}) do
     struct
+    # |> This operator introduces the expression on the left-hand side as the first argument to the function call on the right-hand side.
     |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+  end
+
+  #Pasamos la estructura de cambios de nuestro usuario a través
+  #del conjunto de cambios, luego seleccionamos el parametro contraseña,
+  #validamos su longitud en invocamos la funcion hash_password
+  def registration_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> chast(params, ~w(password)a, [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> hash_password
+  end
+
+  #Comprueba si el conjunto de cambios es valido y los cambios en la estructura
+  #del usuario contienen: parametro de contraseña
+  defp hash_password(changeset) do
+    case changeset do
+      #Comprueba que en el conjunto de cambios contiene la contraseña
+      #que es modificada.
+      #Si todo es correcto, devuelve la hash de la contraseña con la libreria comeonin
+      #y si no devuelve el conjunto de cambios.
+      %Ecto.Changeset{valid?: true,
+                      changes: %{password: password}} ->
+        put_change(changeset,
+                  :password_hash,
+                  Comeonin.Bcrypt.hashpwsalt(password))
+        _ ->
+          changeset
+    end
   end
 end
